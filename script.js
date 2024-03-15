d04cd1242abffa740eaf392bc68a7e3d15fbaddd
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-16 02:49:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function stringChop(str, size) {
-  // your code here
+    var element = [];
+    var temp = '';
+    
+    for(let i = 0; i < str.length; i++){
+        temp += str[i];
+        if(temp.length == size){
+            element.push(temp);
+            temp = '';
+        }
+    }
+    
+    if(temp !== '') element.push(temp);
+    
+    return element;
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
+
